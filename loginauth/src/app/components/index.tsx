0a654ff91cb3a2923bf8d95d955c04732e6000f2
softage910 <github.training@softage.ai>
2025-03-24 12:21:05 +0530

    updatephase6

--- loginauth/src/app/components/index.tsx
+++ loginauth/src/app/components/index.tsx
@@ -3,7 +3,6 @@ import SignInPage from "./signin";
 import Description from "./description"
 import '../page.css';
 
-
 export default function Login() {
   return(
     <div className="mainsection">
